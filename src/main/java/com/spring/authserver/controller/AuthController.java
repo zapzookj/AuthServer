fa3e7628da48e88e0aba5ff79563c7fed7aaa866
zapzookj <zapzook@naver.com>
2025-08-20 23:43:33 +0900

    fix : API 엔드포인트 수정

--- src/main/java/com/spring/authserver/controller/AuthController.java
+++ src/main/java/com/spring/authserver/controller/AuthController.java
@@ -10,12 +10,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/auth")
 public class AuthController {
     private final AuthService authService;
 
